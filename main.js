f3cdaaf7d8ef78f432b047ae102a1afcf3097d78
Syed Muhammad Suleman <sulemansyedmuhammad41@gmail.com>
2024-03-02 15:47:14 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+// STRING EXAMPLE
+var personName = "Suleman, ";
+var question = "Can you like to learn some python today?";
+console.log("Hello " + personName + question);
+// NUMBER DATA TYPE
+var value1 = 16;
+var value2 = 4;
+console.log(value1 + value2);
+// example boolean data types
+var a = false;
+var b = true;
+console.log("Karachi is the capital of Pakistan :", a);
+console.log("Islamabad is the capital of Pakistan :", b);
+// undefined data types
+var k;
+console.log(typeof (k));
+//null
+var t = null;
+console.log(t);
---
+++ main.ts
@@ -0,0 +1,27 @@
+              // STRING EXAMPLE
+
+let personName = "Suleman, ";
+let question = "Can you like to learn some python today?";
+console.log("Hello "+ personName + question);
+
+               // NUMBER DATA TYPE
+
+let value1: number = 16;
+let value2: number = 4;
+console.log(value1 + value2);
+
+            // example boolean data types
+
+let a:boolean = false;
+let b:boolean = true;
+console.log("Karachi is the capital of Pakistan :", a);
+console.log("Islamabad is the capital of Pakistan :", b);   
+   
+            // undefined data types
+let k;
+console.log(typeof(k));
+     
+              //null
+let t:null = null;
+console.log(t);
+
